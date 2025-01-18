b14af9b96734b6b474d53428af14a5c498b91a71
on05583 <on05583@uga.edu>
2025-01-17 20:40:26 -0500

    Added Comments

--- dynamixel_driver.py
+++ dynamixel_driver.py
@@ -3,7 +3,8 @@
 from utils.dynamixel_handler import DynamixelHandler
 import time
 
-handler = DynamixelHandler(1000000)
+handler = DynamixelHandler(1000000) 
+#All Dynamixel Motors Used and their IDs
 DYN_1 = 8
 DYN_2 = 5
 
